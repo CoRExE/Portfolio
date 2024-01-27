dcbe8b7c0cb09a89a6d3457b50782f2805f4de01
CoRE.ExE <98487589+CoRExE@users.noreply.github.com>
2024-01-27 17:04:28 +0100

    Integration Vue.js

---
+++ components/header.vue
@@ -0,0 +1,12 @@
+<template>
+<header>
+    <nav>
+      <ul>
+        <li><a href="index.html">Accueil</a></li>
+        <li><a href="skills.html">Compétences</a></li>
+        <li><a href="project.html">Projets</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+  </template>
